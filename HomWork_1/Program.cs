3b0784c18217f7133af4f4e1d5bc8ac204bd245e
innas <innas@INNA_SVA>
2024-05-27 21:28:15 +0300

    Add project files.

---
+++ HomWork_1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+string name = "Inna";
+string surname = "Svarytsevych";
+int age = 38;
+string hobby = "читати книги, вивчати нове";
+
+
+Console.WriteLine(name);
+Console.WriteLine(surname);
+Console.WriteLine(age);
+Console.WriteLine(hobby);
+
+string question = "Скільки зірок видно на небі неозброєним оком?";
+string quantity = "~6000";
+Console.WriteLine(question);
+Console.WriteLine(quantity);
+
